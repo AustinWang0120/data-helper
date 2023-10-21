162bb4ef9b7799bf2ef30af3a11ef8b2978984a7
Austin Wang <yyaustin@bu.edu>
2023-10-21 03:58:03 -0400

    Added streamlit analytics

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import streamlit as st
+import streamlit_analytics
 from app.pages import excel_processor, transpose_categorizer, variables_trim
 
-page = st.sidebar.selectbox("选择页面", ["Excel 数据处理器", "Transpose 分类处理器", "Variables 分类器"])
+with streamlit_analytics.track():
+    page = st.sidebar.selectbox("选择页面", ["Excel 数据处理器", "Transpose 分类处理器", "Variables 分类器"])
 
-if page == "Excel 数据处理器":
-    excel_processor.run()
-elif page == "Transpose 分类处理器":
-    transpose_categorizer.run()
-elif page == "Variables 分类器":
-    variables_trim.run()
+    if page == "Excel 数据处理器":
+        excel_processor.run()
+    elif page == "Transpose 分类处理器":
+        transpose_categorizer.run()
+    elif page == "Variables 分类器":
+        variables_trim.run()
